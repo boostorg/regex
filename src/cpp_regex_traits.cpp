09a8e172bd0a38d704bdfb57958acb0c1a1657a3
John Maddock <john@johnmaddock.co.uk>
2003-09-22 10:56:29 +0000

    Fixed MSVC-stlport build issues
    [SVN r20149]

--- src/cpp_regex_traits.cpp
+++ src/cpp_regex_traits.cpp
@@ -254,8 +254,9 @@ message_data<char>::message_data(const std::locale& l, const std::string& regex_
    // STLport users as well (gcc3.1+STLport5), so enable the
    // workaround for all STLport users...
    //
-#if defined(__SGI_STL_PORT) || defined(_STLPORT_VERSION)
+#if (defined(__SGI_STL_PORT) || defined(_STLPORT_VERSION)) && !defined(BOOST_MSVC)
    using namespace std;
+   using stlport::isspace;
 #  define BOOST_REGEX_STD
 #else
 #  define BOOST_REGEX_STD std::
@@ -876,5 +877,6 @@ std::size_t BOOST_REGEX_CALL cpp_regex_traits<wchar_t>::strwiden(wchar_t *s1, st
 } // namespace boost
 
 #endif
+
 
 
