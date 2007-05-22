d44bf8c41d9e04791c1bafae42f0b103b7301eca
John Maddock <john@johnmaddock.co.uk>
2007-05-22 17:24:25 +0000

    Improve ICU support.  See http://svn.boost.org/trac/boost/ticket/976
    [SVN r37741]

--- test/regress/test_icu.cpp
+++ test/regress/test_icu.cpp
@@ -39,8 +39,8 @@ class back_insert_iterator
 {
 private:
    Seq* container;
-   typedef const typename Seq::value_type value_type;
 public:
+   typedef const typename Seq::value_type value_type;
    typedef Seq                  container_type;
    typedef std::output_iterator_tag  iterator_category;
 
@@ -647,3 +647,4 @@ void test_icu(const wchar_t&, const test_invalid_regex_tag&){}
 void test_icu(const wchar_t&, const test_regex_replace_tag&){}
 
 #endif
+
