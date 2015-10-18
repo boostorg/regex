389b9c35e0ae57d61999e3a8fe5f8685b6eae765
jzmaddock <john@johnmaddock.co.uk>
2015-10-18 17:50:40 +0100

    Try and improve ICU detection to avoid "DLL Hell".

--- build/has_icu_test.cpp
+++ build/has_icu_test.cpp
@@ -13,6 +13,7 @@
 #include <unicode/utypes.h>
 #include <unicode/uchar.h>
 #include <unicode/coll.h>
+#include <boost/scoped_ptr.hpp>
 
 #if defined(_MSC_VER) && !defined(_DLL)
 #error "Mixing ICU with a static runtime doesn't work"
@@ -22,5 +23,8 @@ int main()
 {
    UErrorCode err = U_ZERO_ERROR;
    UChar32 c = ::u_charFromName(U_UNICODE_CHAR_NAME, "GREEK SMALL LETTER ALPHA", &err);
+   if(err) return err;
+   U_NAMESPACE_QUALIFIER Locale l;
+   boost::scoped_ptr<U_NAMESPACE_QUALIFIER Collator> p_col(U_NAMESPACE_QUALIFIER Collator::createInstance(l, err));
    return err;
 }
